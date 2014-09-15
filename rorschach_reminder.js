f7bd358bc7ba74e2604d858414d95f9ca65fc4d8
tshibata <staatsschreiber@gmail.com>
2014-09-15 15:04:11 +0200

    the first version

---
+++ rorschach_reminder.js
@@ -0,0 +1,9 @@
+function archipelago_for(domain_name, user_name) {
+  var s = domain_name + "/" + user_name;
+  var code = 0;
+  for (var i = 0; i < s.length; i++) {
+    code = (code * 31 + s.charCodeAt(i)) & 0xFFFFFFFF;
+  }
+  return Archipelago(code & 0x7FFFFFFF);
+}
+
